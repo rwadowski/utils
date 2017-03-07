a7b5362163a669df225557aade0ac9b7f09b4560
rwadowski <robert.wadowski@morgenrete.com>
2017-03-07 22:30:22 +0100

    Command line branch visible setup script added

---
+++ git/setup_cmd_tag.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "parse_git_branch() { " >> ~/.bashrc
+echo "git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'" >> ~/.bashrc
+echo " } " >> ~/.bashrc
+echo 'export PS1="${debian_chroot:+($debian_chroot)}\u@\h:\w\$(parse_git_branch)$ "' >> ~/.bashrc
