054df1e0ba5681e0ee005fdb3a91c206d2bbfb7a
Robert Wadowski <robertwadowski@o2.pl>
2017-03-08 23:08:15 +0100

    Added sourcing script

--- git/setup_cmd_tag.sh
+++ git/setup_cmd_tag.sh
@@ -4,3 +4,4 @@ echo "parse_git_branch() { " >> ~/.bashrc
 echo "git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'" >> ~/.bashrc
 echo " } " >> ~/.bashrc
 echo 'export PS1="${debian_chroot:+($debian_chroot)}\u@\h:\w\$(parse_git_branch)$ "' >> ~/.bashrc
+source ~/.bashrc
