25de789fca997fa0ceec0bf63bea403cb8782cf0
Robert Wadowski <robert.wadowski1@gmail.com>
2019-11-09 00:06:40 +0100

    Creation of destination path of unpacked archive

--- git/setup_p4merge.sh
+++ git/setup_p4merge.sh
@@ -2,20 +2,20 @@
 _P4MERGE_PATH="$1"
    
 echo 'Unpacking p4merge'
-tar xvf $_P4MERGE_PATH -C /usr/local/lib/p4merge --strip-components 1
+mkdir -p /usr/local/lib/p4merge & tar xvf $_P4MERGE_PATH -C /usr/local/lib/p4merge --strip-components 1
 
 echo 'Creating p4diff script'    
-rm /usr/local/bin/p4diff
+rm -f /usr/local/bin/p4diff
 echo '#!/bin/sh' >> /usr/local/bin/p4diff
 echo '[ $# -eq 7 ] && /usr/local/bin/p4merge "$2" "$PWD/$5"' >> /usr/local/bin/p4diff
 chmod +x /usr/local/bin/p4diff
 
 echo 'Creating symbolic link to p4 installation binary'
-rm /usr/bin/p4merge
+rm -f /usr/bin/p4merge
 ln -s /usr/local/lib/p4merge/bin/p4merge /usr/bin/p4merge
 
 echo 'Creating p4merge script'
-rm /usr/local/bin/p4merge
+rm -f /usr/local/bin/p4merge
 echo '#!/bin/sh' >> /usr/local/bin/p4merge
 echo '/usr/bin/p4merge $*' >> /usr/local/bin/p4merge
 chmod +x /usr/local/bin/p4merge
